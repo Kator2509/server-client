025e222d6a99548003abf0af82cc39cf0ecf4e82
Kator2509 <139444777+Kator2509@users.noreply.github.com>
2025-05-21 12:32:04 +0300

    Update Client.java
    Добавлены комментарии к строкам.

--- Client.java
+++ Client.java
@@ -8,16 +8,20 @@ public class Client
 {
     public static void main(String[] args)
     {
+        //Trying to connect the server.
         try(Socket server = new Socket("127.0.0.1", 25565)) {
             DataOutputStream out;
+            //if connected to server next.
             if (server.isConnected())
             {
                 out = new DataOutputStream(server.getOutputStream());
 
+                //Getting ip-address from client.
                 try {
                     final DatagramSocket socket = new DatagramSocket();
                     socket.connect(InetAddress.getByName("8.8.8.8"), 10002);
                     InetAddress ip = socket.getLocalAddress();
+                    //Send address to server.
                     out.writeUTF(String.valueOf(ip).substring(1));
                 }
                 catch (IOException e)
